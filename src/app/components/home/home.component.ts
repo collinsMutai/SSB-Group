62782c0d23d2999ee17d5c11aa8a59420eb67436
Collins Mutai <collinsfrontend@gmail.com>
2025-03-29 13:23:48 +0300

    ui

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,11 +4,12 @@ import { ContactComponent } from "../contact/contact.component";
 import { ServicesComponent } from "../services/services.component";
 import { FooterComponent } from "../footer/footer.component";
 import { SectorsComponent } from "../sectors/sectors.component";
+import { AboutComponent } from "../about/about.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroSliderComponent, ContactComponent, ServicesComponent, FooterComponent, SectorsComponent],
+  imports: [HeroSliderComponent, ContactComponent, ServicesComponent, FooterComponent, SectorsComponent, AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
