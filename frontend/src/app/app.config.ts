50b7ad9bc51b6be8ce367e00ae519fadbcde6426
Collins Mutai <collinsfrontend@gmail.com>
2025-10-04 20:40:25 +0300

    v2 fix

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { RECAPTCHA_V3_SITE_KEY } from 'ng-recaptcha';
 
 import { routes } from './app.routes';
 import { environment } from '../environments/environment'; // adjust path if needed
@@ -9,9 +8,5 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    {
-      provide: RECAPTCHA_V3_SITE_KEY,
-      useValue: environment.recaptchaSiteKey
-    }
-  ]
+  ],
 };
